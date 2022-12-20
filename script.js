8fee07b6e5d368b62caa09c2b1203b8fa647e8f4
Jason_Wall <Jason.Waldick@gmail.com>
2022-12-19 23:19:27 -0700

    JS: Board status function created

--- script.js
+++ script.js
@@ -4,31 +4,46 @@ const game_space = document.getElementById('game_space');
 let gridSize = 10;
 makeGrid(gridSize);
 
+//Test cases
+document.getElementById(12).classList.add('active');
+document.getElementById(13).classList.add('active');
+document.getElementById(22).classList.add('active');
+document.getElementById(24).classList.add('active');
+document.getElementById(25).classList.add('active');
 
-
-
-// function clearGrid(){
-// while (game_space.firstChild){
-//     game_space.firstChild.remove();
-// }
-// }
 
 function makeGrid(grid){
-let n = 0;
-const row = [grid];
-const cell = [grid*grid];
-for (let i = 0; i < grid; i++){
-    row[i] = document.createElement('div');
-    row[i].classList.add('row');
-    game_space.appendChild(row[i]);
-    for (let j = 0; j < grid; j++){
-        cell[n] = document.createElement('div');
-        row[i].appendChild(cell[n]);
-        cell[n].textContent = n;
-        cell[n].classList.add('cell');
-        n++;
+    let n = 0;
+    const row = [grid];
+    const cell = [grid*grid];
+    for (let i = 0; i < grid; i++){
+        row[i] = document.createElement('div');
+        row[i].classList.add('row');
+        game_space.appendChild(row[i]);
+        for (let j = 0; j < grid; j++){
+            cell[n] = document.createElement('div');
+            row[i].appendChild(cell[n]);
+            // cell[n].textContent = n;
+            cell[n].classList.add('cell')
+            cell[n].id = n;
+            n++;
+        }
     }
 }
+
+function boardstate(){
+    const allCells = document.querySelectorAll(".cell");
+    let cellsArr = [... allCells];
+    let activeCells = [];
+    for (i in cellsArr){
+        if (cellsArr[i].classList.contains('active')){
+            activeCells.push(i);
+        }
+    }
+    return(activeCells);
 }
+
+
+
 
 
